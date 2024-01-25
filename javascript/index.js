bc469ed4a465db57bcfdaa1afa8908342d98d75e
SaranyaMohan1990 <144958996+SaranyaMohan1990@users.noreply.github.com>
2024-01-25 07:27:16 -0800

    added time and date update

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,22 @@
+function timeUpdate() {
+  let newyorkElement = document.querySelector("#new-york");
 
+  let newyorkDateElement = newyorkElement.querySelector(".date");
+  let newyorkTimeElement = newyorkElement.querySelector(".time");
+  let newyorkTime = moment().tz("EST");
+  newyorkDateElement.innerHTML = newyorkTime.format("MMMM Do YYYY");
+  newyorkTimeElement.innerHTML = `${newyorkTime.format(
+    "h:mm:ss"
+  )} <small>${newyorkTime.format("A")}</small>`;
+  let sydneyElement = document.querySelector("#sydney");
+
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = `${sydneyTime.format(
+    "h:mm:ss"
+  )} <small>${sydneyTime.format("A")}</small>`;
+}
+timeUpdate();
+setInterval(timeUpdate, 1000);
